663c9530fd2f0483b1259f64ea7471b31ec81d99
Kriger Gábor <regirck@gmail.com>
2019-03-12 17:33:22 +0100

    initialize repository

---
+++ src/main/java/com/codecool/springbootfaragobalazsyt/SpringBootFaragoBalazsYtApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.springbootfaragobalazsyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFaragoBalazsYtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFaragoBalazsYtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/springbootfaragobalazsyt/SpringBootFaragoBalazsYtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecool.springbootfaragobalazsyt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootFaragoBalazsYtApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
